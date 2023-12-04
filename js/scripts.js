cab290b69267136aff1477bf426654d5eb4e4c48
magaliehameyie <magaliehameyie@gmail.com>
2023-12-03 23:49:12 -0500

    correcting errors week8

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
+var mainNav = document.querySelector("#main-nav");
+var signInUpButtons = document.querySelector("#sign-in-up-buttons");
+var burgerBtn = document.querySelector("#burger-container button");
 
-var mainNav = document.querySelector("#mainNav");
+var burgerBars = document.querySelector("#burger-bars");
+var burgerX = document.querySelector("#burger-x");
 
-var burgerBtn = document.querySelector("#burger-container button")
 
-var canYouSeeMainNav = false;;
+
+var canYouSeeMainNav = false;
 
 function showNav(){
     
@@ -11,14 +15,63 @@ function showNav(){
         console.log("show menu");
         
         mainNav.classList.add("showMainNav");
+
+        signInUpButtons.classList.add("showSignUp");
         canYouSeeMainNav = true;
+
+        
+        burgerBars.classList.add("hide-burger-bars");
+
+        
+        burgerX.classList.remove("hide-burger-x");
+
     }else{
         console.log("hide menu");
-        
+       
         mainNav.classList.remove("showMainNav");
+
+        signInUpButtons.classList.remove("showSignUp");
+
+        
+        burgerBars.classList.remove("hide-burger-bars");
+
+        
+        burgerX.classList.add("hide-burger-x");
+
         canYouSeeMainNav = false;
     }
     
 }
 
+
 burgerBtn.addEventListener("click", showNav);
+
+
+
+/* --------------------
+
+    Sign UP
+ -------------------- */  
+
+ var signUpBtn = document.querySelector("#sign-up-btn");
+ var signUpContainer = document.querySelector("#sign-up-container");
+ var closeSignUpModalBtn = document.querySelector("#close-sign-up-modal-btn");
+
+
+ function showSignUpModal() {
+    // console.log("show modal now");
+    signUpContainer.classList.add("show-signUp-container");
+    // hide the navigation on close
+    mainNav.classList.remove("showMainNav");
+    // hide the signup/in buttons
+    signInUpButtons.classList.remove("showSignUp");
+ }
+
+ function hideSignUpModal(){
+    signUpContainer.classList.remove("show-signUp-container");
+    
+ }
+
+ signUpBtn.addEventListener("click", showSignUpModal);
+ closeSignUpModalBtn.addEventListener("click", hideSignUpModal);
+ signUpContainer.addEventListener("click", hideSignUpModal);
